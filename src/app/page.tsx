440d5dd54159c0ca80af90e8659f548abca23e4b
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-29 19:32:04 +0800

    Add ServicesSection to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/organisms/about-section";
 import HeaderSection from "@/components/organisms/header-section";
+import ServicesSection from "@/components/organisms/services-section";
 
 export default function Home() {
   return (
     <>
       <HeaderSection />
       <AboutSection />
+      <ServicesSection />
     </>
   );
 }
