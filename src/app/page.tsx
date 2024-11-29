7679b75122b579cf9ed5571ec3b658f86db96667
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-29 18:14:58 +0800

    Add about-section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "@/components/organisms/about-section";
 import HeaderSection from "@/components/organisms/header-section";
 
 export default function Home() {
   return (
     <>
       <HeaderSection />
+      <AboutSection />
     </>
   );
 }
