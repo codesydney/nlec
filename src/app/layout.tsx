4c76100f47f4070f7dbf9273631fc20cb08393e1
Sly Villafania <villafania.sly.lau@gmail.com>
2024-11-29 16:49:02 +0800

    Add scroll-smooth css Edit main element to have proper top padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/organisms/navbar";
 import { cn } from "@/lib/utils";
 import { inter } from "@/assets/fonts";
 
@@ -15,10 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={cn("antialiased", inter.className)}>
         <Navbar />
-        <main className="mt-12">{children}</main>
+        <main className="mt-[65px] h-[3000px] md:mt-[69px] xl:mt-[73px]">
+          {children}
+        </main>
       </body>
     </html>
   );
