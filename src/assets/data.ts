bb72b644fc1c3e903b5fb60244906837e54b9a73
Sly Villafania <villafania.sly.lau@gmail.com>
2025-03-11 07:16:54 +0800

    Comment out unnecessary links in join us section

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,6 +1,6 @@
-import Fb from "@/public/fb";
-import Twitter from "@/public/twitter";
-import Instagram from "@/public/instagram";
+// import Fb from "@/public/fb";
+// import Twitter from "@/public/twitter";
+// import Instagram from "@/public/instagram";
 
 export const FOOTER_ITEMS = {
   links: [
@@ -9,25 +9,25 @@ export const FOOTER_ITEMS = {
       content: "tina@justicereforminitiative.org.au",
       href: "mailto:tina@justicereforminitiative.org.au",
     },
-    {
-      label: "Phone",
-      content: "+61 123 456 789",
-      href: "tel:+61123456789",
-    },
-    {
-      label: "Address",
-      content: "Canberra, ACT 2600, Australia",
-      href: "https://maps.app.goo.gl/xDYHRutBSzwCfagf7",
-    },
+    // {
+    //   label: "Phone",
+    //   content: "+61 123 456 789",
+    //   href: "tel:+61123456789",
+    // },
+    // {
+    //   label: "Address",
+    //   content: "Canberra, ACT 2600, Australia",
+    //   href: "https://maps.app.goo.gl/xDYHRutBSzwCfagf7",
+    // },
   ],
-  socials: [
-    { icon: Fb, href: "#" },
-    { icon: Twitter, href: "#" },
-    { icon: Instagram, href: "#" },
-  ],
+  // socials: [
+  //   { icon: Fb, href: "#" },
+  //   { icon: Twitter, href: "#" },
+  //   { icon: Instagram, href: "#" },
+  // ],
   "TOS-PP": [
     { label: "Privacy Policy", href: "#" },
-    { label: "Terms Of Use", href: "#" },
+    // { label: "Terms Of Use", href: "#" },
   ],
 };
 
